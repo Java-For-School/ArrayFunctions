6b05a5e2164e12269cb0365a82525ab431b3ff61
SorerBOT <alonfiller@gmail.com>
2023-04-08 21:17:29 +0300

    first commit

---
+++ createArray/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+  public static void main(String[] args) {
+    int[] array = createArray(8);
+
+    System.out.println(array);
+  }
+  public static int[] createArray(int length) {
+    return new int[length];
+  } 
+}
